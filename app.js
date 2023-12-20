b3674860b1247b46a1a7927f0c9651f00e4ac217
Abimbola Oladejo <abimbolaoladejo4@gmail.com>
2023-12-20 16:13:21 +0100

    working on functionalities

--- app.js
+++ app.js
@@ -1,100 +1,45 @@
-// notification alert
-const notificationContainer = document.getElementById('notificationInfo'); 
-const notificationMenu = document.getElementById("notificationMenu");
-function toggleAlertInfo(){
-    notificationContainer.classList.toggle('open-notification');
-    const notificationExpanded = notificationMenu.attributes["aria-expanded"].value == "true";
-    if(notificationExpanded){
-        notificationMenu.ariaExpanded = "false";
-    }
-    else{
-        notificationMenu.ariaExpanded = "true";
-    } 
-    console.log("working")
-}
-notificationContainer.addEventListener('click', toggleAlertInfo)
+/*
+HEADER BUTTONS
+--------------------------
+This section handles the header buttons and the button card info visibility
+*/
+let headerButtons = document.querySelectorAll('.merchant-goto > button');
+headerButtons.forEach(button => {
+    let menuCard = document.getElementById(button.getAttribute('aria-controls'));
 
+    button.addEventListener('click', e =>{
+        e.stopPropagation(); //to make the parent unaware that it was clicked
 
-// functions for user profile  submenu and arrow key functionality
-function ProfileMenu(){
-    const profileMenuBtn = document.querySelector("#m-profile");
-    const profileSubMenu = document.querySelector("#profileSubMenu");
-    const allMenuItem = profileSubMenu.querySelectorAll('[role="menuitem"]');
-
-    function closeMenu(){
-        profileMenuBtn.ariaExpanded = "false";
-        profileMenuBtn.focus()
-    }
-
-    function handleMenuEscapeKeyPress(event){
-        if(event.key == "Escape"){
-            toggleProfileMenu()
-        }
-    }
-
-    function handleMenuItemArrowKeyPress(event, menuItemIndex){
-        // console.log(event)
-        const isLastMenuItem = menuItemIndex === allMenuItem.length - 1;
-        const isFirstMenuItem = menuItemIndex === 0;
-        const nextMenuItem = allMenuItem.item(menuItemIndex + 1);
-        const previousMenuItem = allMenuItem.item(menuItemIndex - 1);
-        // arrow up or left focus on next menu item
-    
-        // arrow down or right focus on previous menu item
-        if (event.key === "ArrowRight" ||  event.key === "ArrowDown"){
-            if(isLastMenuItem){
-                allMenuItem.item(0).focus();
-                return;
+        // reset all button's aria-expanded except current clicked button
+        headerButtons.forEach(buttonToClose => {
+            if(buttonToClose !== button){
+                buttonToClose.ariaExpanded = false;
             }
-            nextMenuItem.focus();
-            console.log("Not working")
-        };
-         // if the user pressed arrow up or arrow left
-        if (event.key === "ArrowUp" || event.key === "ArrowLeft"){
-            if (isFirstMenuItem) {
-            allMenuItem.item(allMenuItem.length - 1).focus();
-            return;
-            }
-             previousMenuItem.focus();
-        };
-    
-    }
-
-    function OpenMenu(){
-        profileMenuBtn.ariaExpanded = "true";
-        // getting all items in the menu
-     
-        allMenuItem.item(0).focus();
+        })
+        // Toggle the aria-expanded state
+        button.ariaExpanded = !JSON.parse(button.ariaExpanded);
         
-        profileSubMenu.addEventListener("keyup", handleMenuEscapeKeyPress);
-        // toggling each item using arrows
-        allMenuItem.forEach(function(menuItem, menuItemIndex){
-            menuItem.addEventListener("keyup", 
-            function(event){
-                handleMenuItemArrowKeyPress(event, menuItemIndex);
-            });
-        });
-    }
-    function toggleProfileMenu(){
-        profileSubMenu.classList.toggle("menu-active");
-        const menuExpanded = profileMenuBtn.attributes["aria-expanded"].value == "true";
-        if(menuExpanded){
-            closeMenu();
+        // focus automatically on an element when the button is clicked
+        let firstFocus = menuCard.querySelector('[role="menu-item"], a , button ');
+        // Focus on the first element if one is found
+        firstFocus && firstFocus.focus();
+    });
+
+    //close the card when a clicked is made on the website
+    document.addEventListener("click", function(e){
+        let clickedElement = e.target;
+        if(menuCard && !menuCard.contains(clickedElement)){
+            button.ariaExpanded = false;
         }
-        else{
-            OpenMenu();
-        };
-        
-    }
-    
-    profileMenuBtn.addEventListener("click", toggleProfileMenu);
-    
-}
-ProfileMenu();
+    })
+})
 
 
-
-// closing plan for free trial container
+/*
+CLOSE TRIAL BUTTON
+--------------------------
+This section handles the function of closing the plan free trial 
+*/
 let planContainer = document.getElementById('plan-container');
 let closeBtn = document.getElementById("close-trial-l"); //this is for laptop screen
 let closeBtnS = document.getElementById("close-trial-s"); //this is for mobile screens
@@ -106,7 +51,12 @@ closeBtnS.addEventListener("click", function(){
     planContainer.style.display = "none"
 })
 
-// toggling setUpGuideInfo using the dropdownIcon
+
+/*
+TOGGLE SETUP VISIBILITY
+--------------------------
+Toggling setUpGuideInfo using the dropdownIcon 
+*/
 function hideShow(){
     var setupGuideInfo = document.getElementById("setup-guide-info");
     var dropdownOpen = document.getElementById("open");
@@ -134,25 +84,20 @@ function hideShow(){
     dropdownGuide.addEventListener("click", toggleGuide )
 }
 
+/*
+ACCORDION
+--------------------------
+Toggling the guide steps
+*/
+Array.from(document.getElementsByClassName('guide')).forEach(step=>{
+    let checkbox = step.querySelector('button.check-button');
+    let guideHeading = step.querySelector('.guide-title');
+    let steps = step.parentNode.querySelectorAll('.guide');
+    let guideContent = step.querySelector('.guide-content')
 
 
-// default circle spinner
-btn = document.querySelector(".check-button");
-spinner = document.querySelector(".spinner");
-stepDone = document.querySelector(".complete");
-icon = document.querySelector("svg")
-btn.onclick = function(){
-    btn.style.cursor = "wait";
-    icon.classList.replace(".default-circle", ".spinner");
-    console.log("working")
 
-    setTimeout(()=> {
-        btn.style.pointerEvents = "none";
-        icon.classList.replace(".spinner", ".complete")
-        console.log("not")
-    }, 4000)
-}
-
+})
 // setup guide toggling
 function openContent(step){
     var i;
